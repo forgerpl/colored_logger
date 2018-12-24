3158936122b3a7c6f6ec2b2b3ebbc99340e63101
Jacek Chmiel <jchmiel@collective-sense.com>
2018-12-24 13:32:55 +0100

    Add selectable coloring functionality
    This introduces feature that allows user to select coloring behavior
    as auto, never and always. Auto allows to automatically use colors
    when writing to a terminal that supports colors. When stderr is
    redirected (pipe or to file), colors are suppressed in auto mode.

--- src/lib.rs
+++ src/lib.rs
@@ -4,50 +4,102 @@
 //!
 //! # Examples
 //!
-//! ```no_run
-//! #[macro_use]
-//! extern crate log;
-//! extern crate flexi_logger;
-//! extern crate colored_logger;
-//!
-//! use colored_logger::formatter;
-//!
-//! fn main() {
-//!    flexi_logger::LogOptions::new()
-//!        .format(formatter)
-//!        .init(Some("info".to_string()))
-//!        .unwrap();
-//!
-//!
-//!    info!("This is a test message");
-//!    error!("Error!");
-//! }
-//! ```
+//! See examples/auto.rs
 
 extern crate chrono;
 extern crate colored;
 extern crate flexi_logger;
 
+use atty;
 use chrono::Local;
 use colored::{ColoredString, Colorize};
 use flexi_logger::{Level, Record};
+use std::env;
 
-pub fn formatter(w: &mut std::io::Write, record: &Record) -> Result<(), std::io::Error> {
-    let level = record.level();
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum ColorChoice {
+    /// Always emit colors.
+    Always,
 
-    fn color<T: ToString>(fstr: &T, level: Level) -> ColoredString {
-        use self::Level::*;
-        let fstr = fstr.to_string();
-        let fstr = fstr.as_str();
+    /// Use colors on decent terminal output. Don't use on dumb terminal or
+    /// redirected stderr.
+    Auto,
 
-        match level {
-            Error => fstr.red(),
-            Warn => fstr.yellow(),
-            Info => fstr.green(),
-            Debug => fstr.blue(),
-            Trace => fstr.magenta(),
+    /// Never emit colors.
+    Never,
+}
+
+impl ColorChoice {
+    /// Returns true if we should attempt to write colored output.
+    fn should_attempt_color(&self) -> bool {
+        match *self {
+            ColorChoice::Always => true,
+            ColorChoice::Never => false,
+            ColorChoice::Auto => {
+                if atty::is(atty::Stream::Stderr) {
+                    match env::var("TERM") {
+                        Err(_) => false,
+                        Ok(k) => k != "dumb",
+                    }
+                } else {
+                    false
+                }
+            }
         }
     }
+}
+
+pub struct FormatterBuilder {
+    color: ColorChoice,
+}
+
+impl FormatterBuilder {
+    pub fn new() -> Self {
+        FormatterBuilder {
+            color: ColorChoice::Auto,
+        }
+    }
+
+    pub fn with_color(mut self, color: ColorChoice) -> Self {
+        self.color = color;
+        self
+    }
+
+    pub fn build(self) -> fn(&mut std::io::Write, &Record) -> Result<(), std::io::Error> {
+        if self.color.should_attempt_color() {
+            color_formatter
+        } else {
+            no_color_formatter
+        }
+    }
+}
+impl Default for FormatterBuilder {
+    fn default() -> Self {
+        FormatterBuilder::new()
+    }
+}
+
+fn color<T: ToString>(fstr: &T, level: Level) -> ColoredString {
+    use self::Level::*;
+    let fstr = fstr.to_string();
+    let fstr = fstr.as_str();
+
+    match level {
+        Error => fstr.red(),
+        Warn => fstr.yellow(),
+        Info => fstr.green(),
+        Debug => fstr.blue(),
+        Trace => fstr.magenta(),
+    }
+}
+
+#[deprecated(since = "0.4.0", note = "use FormatterBuilder instead")]
+pub fn formatter(w: &mut std::io::Write, record: &Record) -> Result<(), std::io::Error> {
+    color_formatter(w, record)
+}
+
+fn color_formatter(w: &mut std::io::Write, record: &Record) -> Result<(), std::io::Error> {
+    let level = record.level();
 
     write!(
         w,
@@ -56,6 +108,20 @@ pub fn formatter(w: &mut std::io::Write, record: &Record) -> Result<(), std::io:
         color(&level, level),
         color(&record.file().unwrap_or_default(), level),
         color(&record.line().unwrap_or_default(), level),
+        &record.args()
+    )
+}
+
+fn no_color_formatter(w: &mut std::io::Write, record: &Record) -> Result<(), std::io::Error> {
+    let level = record.level();
+
+    write!(
+        w,
+        "[{}] {} [{}:{}] {}",
+        &Local::now().format("%Y-%m-%d %H:%M:%S%.6f %:z"),
+        &level,
+        &record.file().unwrap_or_default(),
+        &record.line().unwrap_or_default(),
         &record.args()
     )
 }
